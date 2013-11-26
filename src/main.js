f40078a1ea3cf4b326a504412aaff84a7f8963db
Benjamin Landers <benjaminrlanders@gmail.com>
2013-11-26 14:48:30 -0800

    added commit stuff

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-function ghapi(user,repo){
+function ghapi(user,repo,pass){
   this.user = user;
+  this.password = pass;
   this.repo = repo;
 }
 ghapi.prototype.getData = function(url){
@@ -7,6 +8,8 @@ ghapi.prototype.getData = function(url){
     type: "GET",
     url: 'https://api.github.com/repos/'+this.user+'/'+this.repo+url,
     crossDomain: true,
+    username: this.user,
+    password: this.password,
     async:false,
     failure: function(){console.log('error');},
     dataType: 'json'
@@ -15,6 +18,18 @@ ghapi.prototype.getData = function(url){
 ghapi.prototype.getCommitSha = function(ref){
   return this.getData('/git/refs/heads/'+ ref).object.sha;
 };
+ghapi.prototype.getCommit = function(sha){
+  return  this.getData('/git/commits/'+sha);
+};
+ghapi.prototype.makeParentArray = function(commit,addthis){
+  console.log(commit)
+  var out = [];
+  for(var i=0;i < commit.parents.length;i++)
+    out.push(commit.parents[i].sha);
+  if(addthis)
+    out.push(commit.sha);
+  return out;
+};
 a = new ghapi('thethirdone','github-api-testing');
 //console.log('hello',a.getData('/git/refs/heads/master',function(data){console.log(data)}));
-console.log(a.getCommitSha('master'));
+console.log(a.makeParentArray(a.getCommit(a.getCommitSha('master')),true));
