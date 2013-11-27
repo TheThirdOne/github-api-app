ab9b1192304abd71af5cc74d58171279018c8df0
Benjamin Landers <benjaminrlanders@gmail.com>
2013-11-26 16:44:38 -0800

    added abstraction + made a commit!

--- src/main.js
+++ src/main.js
@@ -14,10 +14,17 @@ function make_base_auth(user, password) {
   return "Basic " + hash;
 }
 ghapi.prototype.getData = function(url){
+  return this.send(url,{},"GET");
+};
+ghapi.prototype.putData = function(url,data){
+  return this.send(url,data,"POST");
+};
+ghapi.prototype.send = function(url,data,type){
   return $.ajax({
-    type: "GET",
+    type: type,
     url: 'https://api.github.com/repos/'+this.user+'/'+this.repo+url,
     crossDomain: true,
+    data:data,
     username: this.user,
     password: this.password,
     async:false,
@@ -56,3 +63,18 @@ $('input#user').val(localStorage.getItem('user'));
 $('input#repo').val(localStorage.getItem('repo'));
 $('input#pass').val(localStorage.getItem('pass'));
 
+function go(){
+  commit = a.getCommit(a.getCommitSha('master'));
+  parents = commit.sha;
+  tree_base = commit.tree.sha;
+  /*console.log(a.putData("/git/commits",JSON.stringify({"message": "my commit message",
+  "author": {
+    "name": "Scott Chacon",
+    "email": "schacon@gmail.com",
+    "date": "2008-07-09T16:13:30+12:00"
+  },
+  "parents":[parents],"tree":tree_base})));
+  console.log(a.putData("/git/refs/heads/master",JSON.stringify({
+  "sha": "2ce41964c465ac08045b135f68d9fe39317ebcf0"
+})));*/
+}
